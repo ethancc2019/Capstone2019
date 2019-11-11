e3b5a28a966377f0c719e25218fe9621d13b1a3b
Ethan C <ethan.collins335@myci.csuci.edu>
2019-11-11 13:15:34 -0800

    Working on Game two

--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #2/GameTwoMovement.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #2/GameTwoMovement.cs
@@ -14,7 +14,19 @@ public class GameTwoMovement : MonoBehaviour {
     public float rotationDrag = 1;
 
     private Vector3 velocity;
-    private float zRotationVelocity;
+    private float rotationVelocity;
+
+    public GameObject bulletPrefab;
+    public Transform point;
+    private Rigidbody2D rb;
+    public float velocityX = 0f;
+    public float velocityY = 5f;
+
+
+    void Start()
+    {
+        //Camera.main.enabled = true;
+    }
 
     private void Update()
     {
@@ -23,8 +35,30 @@ public class GameTwoMovement : MonoBehaviour {
         velocity += acceleration * Time.deltaTime;
 
         // apply turn input
-        float zTurnAcceleration = -1 * Input.GetAxis("Horizontal") * horizontalInputAcceleration;
-        zRotationVelocity += zTurnAcceleration * Time.deltaTime;
+        float turnAccleration = -1 * Input.GetAxis("Horizontal") * horizontalInputAcceleration;
+        rotationVelocity += turnAccleration * Time.deltaTime;
+
+        //Taking care of keeping th e player in the screen bounds
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+        if (Input.GetKeyDown(KeyCode.Space)) //Can make this automatic firing if we want 
+        {
+            Shoot();
+        }
+    }
+
+
+    public void Shoot()
+    {
+        Debug.Log("Pew Pew!");
+        Instantiate(bulletPrefab, point.position, Quaternion.identity);
+        rb = GameObject.Find("Bullet(Clone)").GetComponent<Rigidbody2D>();
+
+        rb.velocity = new Vector2(velocityX, velocityY);
+
     }
 
     private void FixedUpdate()
@@ -36,13 +70,13 @@ public class GameTwoMovement : MonoBehaviour {
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
 
         // apply rotation drag
-        zRotationVelocity = zRotationVelocity * (1 - Time.deltaTime * rotationDrag);
+        rotationVelocity = rotationVelocity * (1 - Time.deltaTime * rotationDrag);
 
         // clamp to maxRotationSpeed
-        zRotationVelocity = Mathf.Clamp(zRotationVelocity, -maxRotationSpeed, maxRotationSpeed);
+        rotationVelocity = Mathf.Clamp(rotationVelocity, -maxRotationSpeed, maxRotationSpeed);
 
         // update transform
         transform.position += velocity * Time.deltaTime;
-        transform.Rotate(0, 0, zRotationVelocity * Time.deltaTime);
+        transform.Rotate(0, 0, rotationVelocity * Time.deltaTime);
     }
 }
