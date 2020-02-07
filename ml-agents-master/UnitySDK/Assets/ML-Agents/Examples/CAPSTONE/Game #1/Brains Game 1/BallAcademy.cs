3c6d00bd5e07f1c3b60d2675c77b817d7a6a9e3f
John Storie <johnhstorie@gmail.com>
2020-02-07 14:58:00 -0800

    Big changes to agent

--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #1/Brains Game 1/BallAcademy.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #1/Brains Game 1/BallAcademy.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAgents;
+public class BallAcademy : Academy {
 
-public class BallAcademy : MonoBehaviour {
+	public override void AcademyReset()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #1/Brains Game 1/BallAgent.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #1/Brains Game 1/BallAgent.cs
@@ -6,30 +6,83 @@ using MLAgents;
 public class BallAgent : Agent
 {
     public Rigidbody rigidbody;
+    private RayPerception3D rayPerception;
+    public Transform Target;
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    public Transform spawnPointOne;
+    public GameObject gameManger;
+    private CharacterController controller;
     // Use this for initialization
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-    }
-
-    public Transform Target;
-    public override void AgentReset()
-    {
-
-    }
-
-    public override void CollectObservations()
-    {
-        base.CollectObservations();
-        AddVectorObs(this.transform.position);
-        AddVectorObs(Target.position);
-        AddVectorObs(rigidbody.velocity.x);
-        AddVectorObs(rigidbody.velocity.y);
-        AddVectorObs(rigidbody.velocity.z);
+        rayPerception = GetComponent<RayPerception3D>();
+        controller = GetComponent<CharacterController>();
     }
 
     public override void AgentAction(float[] vectorAction, string textAction)
     {
-        base.AgentAction(vectorAction, textAction);
+        // is the controller on the ground?
+        if (controller.isGrounded)
+        {
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(vectorAction[1], 0, vectorAction[0]);
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            //Jumping
+            if (vectorAction[2] == 1)
+                moveDirection.y = jumpSpeed;
+
+        }
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //Player fell off platform, reset him to the first spawn point
+        if (this.transform.position.y <= -1)
+        {
+            AddReward(-1f);
+            AgentReset();
+        }
     }
+    public override void AgentReset()
+    {
+        this.transform.position = spawnPointOne.position;
+    }
+
+    public override void CollectObservations()
+    {
+        AddVectorObs(this.transform.position);
+        AddVectorObs(Target.position);
+
+        //Rayperception
+        float rayDistance = 20f;
+        float[] rayAngles = { 30f, 60f, 90f, 120f, 150f, 180f, 210f, 240f, 270f, 300f, 330f, 360f };
+        string[] detectableObjects = { "killWall", "goal" };
+        AddVectorObs(rayPerception.Perceive(rayDistance, rayAngles, detectableObjects, 0f, 0f));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("killWall"))
+        {
+            AddReward(-1f);
+            AgentReset();
+        }
+        else if (collision.transform.CompareTag("goal"))
+        {
+            AddReward(1f);
+            AgentReset();
+        }
+    }
+
+    
 }
