41ec1e321b36be5b8961367aa49e322258792e6c
Ethan Collins <ethan.collins335@myci.csuci.edu>
2020-02-13 16:00:21 -0800

    Testing Training

--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #1/Scripts/Game1Agent.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #1/Scripts/Game1Agent.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using MLAgents;
 using System.Linq;
 using System;
-using TMPro;
 public class Game1Agent : Agent
 {
     // Start is called before the first frame update
     private RayPerception3D rayPerception;
     private CharacterController controller;
     private GameObject currentGoal;
+    private GameObject floorObj;
     private TextMesh cumulativeRewardText;
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+    private GameObject spawnPointOne;
+    private Transform startPosition;
     public override void AgentAction(float[] vectorAction, string textAction)
     {
         float forward = 0f;
@@ -73,7 +73,7 @@ public class Game1Agent : Agent
     public override void AgentReset()
     {
         //Score negation, punishment for falling off the edge.
-        GetComponent<PlayerMovement>().ResetPlayer();
+        this.transform.position = spawnPointOne.transform.position;
     }
     public override void CollectObservations()
     {
@@ -84,7 +84,10 @@ public class Game1Agent : Agent
         //TODO: Goal position here needs to be replaced.
         AddVectorObs(Vector3.Distance(currentGoal.transform.position, transform.position));
         //Direction to goal
-        AddVectorObs((transform.position - transform.position).normalized);
+        AddVectorObs((currentGoal.transform.position - transform.position).normalized);
+        AddVectorObs(floorObj.transform.position);
+
+        AddVectorObs(floorObj.transform.localScale);
         //Agent's direction
         AddVectorObs(transform.forward);
 
@@ -97,7 +100,7 @@ public class Game1Agent : Agent
 
         float rayDistance = 10f;
         float[] rayAngles = {0f, 30f, 60f, 90f, 120f, 150f, 180f};
-        string[] detectableObjects = {"wall", "goal", "platform"};
+        string[] detectableObjects = {"wall", "goal", "platform", "floor"};
         //I hypothesize that offsets 0f&0f will need to be changed to be at the level of the ball, should be okay for now.
         AddVectorObs(rayPerception.Perceive(rayDistance, rayAngles, detectableObjects, 0f, 0f));
     }
@@ -106,6 +109,10 @@ public class Game1Agent : Agent
         //controller = GetComponent<CharacterController>();
         rayPerception = GetComponent<RayPerception3D>();
         currentGoal = GetClosestGoal();
+
+        spawnPointOne = GameObject.Find("SP #1");
+        //startPosition.position = this.transform.position;
+        floorObj = GameObject.FindGameObjectWithTag("floor");
     }
     private GameObject GetClosestGoal()
     {
@@ -129,12 +136,13 @@ public class Game1Agent : Agent
     }
     private void FixedUpdate()
     {
-        String rewardStr = String.Format("Reward currently: {0} ", GetCumulativeReward());
-        Debug.Log(rewardStr);
+        //String rewardStr = String.Format("Reward currently: {0} ", GetCumulativeReward());
+        //Debug.Log(rewardStr);
         String value = String.Format("{0}", GetCumulativeReward());
         //cumulativeRewardText.SendMessage(value);
         currentGoal = GetClosestGoal();
 
+        
         //Checks if agent falls off
         if (transform.position.y <= -5)
         {
@@ -142,7 +150,7 @@ public class Game1Agent : Agent
             AgentReset();
         }
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.transform.CompareTag("wall"))
         {
@@ -151,7 +159,9 @@ public class Game1Agent : Agent
         else if (collision.transform.CompareTag("goal"))
         {
             //TODO: Goal gameObject = GetClosestGoal();
+            Debug.Log("Goal Hit!");
             AddReward(1f);
+            AgentReset();
         }
     }
 }
