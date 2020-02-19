820a41b2faa1da642cb5dbc145878e7cac75b7cf
retgaine <retagaine@gmail.com>
2020-02-19 00:00:29 -0800

    no message

--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #1/Scripts/PlayerMovement.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #1/Scripts/PlayerMovement.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -59,60 +58,3 @@ public class PlayerMovement : MonoBehaviour
         this.transform.position = spawnPointOne.transform.position;
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour {
-
-    //Variables
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
-    
-
-    public Transform spawnPointOne;
-
-    //Scripts 
-    public GameObject gameManger;
-
-    void Update()
-    {
-        CharacterController controller = GetComponent<CharacterController>();
-        // is the controller on the ground?
-        if (controller.isGrounded)
-        {
-            //Feed moveDirection with input.
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            //Multiply it by speed.
-            moveDirection *= speed;
-            //Jumping
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
-
-        }
-        //Applying gravity to the controller
-        moveDirection.y -= gravity * Time.deltaTime;
-        //Making the character move
-        controller.Move(moveDirection * Time.deltaTime);
-
-        //Player fell off platform, reset him to the first spawn point
-        if (this.transform.position.y <= -1)
-        {
-            
-            resetPlayer();
-        }
-
-    }
-
-    public void resetPlayer()
-    {
-        gameManger.GetComponent<GameManager>().decreaseScore();
-        this.transform.position = spawnPointOne.position;
-    }
-}
- 
->>>>>>> 26ce902bc527fa4f91fbb129d5b1fd17b3a2e78d
