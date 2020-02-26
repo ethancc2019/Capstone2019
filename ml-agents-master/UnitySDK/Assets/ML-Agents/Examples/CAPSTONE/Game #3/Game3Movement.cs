75cbcee02dba6df735b76e7cccb381e19dd00adf
Ethan C <ethan.collins335@myci.csuci.edu>
2020-02-26 10:42:46 -0800

    Player movement, Shooting, bullet effect, and Destroy logic

---
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #3/Game3Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game3Movement : MonoBehaviour
+{
+
+
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 movement;
+    private Vector2 mousePosition;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        //Mouse rotations
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
