b9744ac3b073908ba4a78e4019998cc8b1659b09
Ethan C <ethan.collins335@myci.csuci.edu>
2020-02-26 10:47:09 -0800

    Organization

---
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #3/Scripts/Shooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Transform firePoint;
+
+    public GameObject bulletPrefab;
+
+    public float bulletSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1")) //Left click
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bulletTemp = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bulletTemp.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+
+    }
+}
