1ef56f93ecbdc2696de68721a9cecc099514deb0
retgaine <retagaine@gmail.com>
2020-03-20 16:54:32 -0700

    Added changing goal size

--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #2/Scripts/Game2Academy.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #2/Scripts/Game2Academy.cs
@@ -7,6 +7,8 @@ public class Game2Academy : Academy
 {
     public override void AcademyReset()
     {
-
+        Asteroid.speed = resetParameters["asteroid_speed"];
+        Asteroid.size = resetParameters["asteroid_size"];
+        PowerUpSpawnner.size = resetParameters["goal_size"];
     }
 }
