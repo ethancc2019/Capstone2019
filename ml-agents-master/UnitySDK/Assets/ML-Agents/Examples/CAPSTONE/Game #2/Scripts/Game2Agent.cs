ddfd219df1830dfceeabbeb827bfa4325fa09e3d
retagaine <casualgamer105@yahoo.com>
2020-04-14 13:12:02 -0700

    Added bullet observations, normalized all observations

--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #2/Scripts/Game2Agent.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #2/Scripts/Game2Agent.cs
@@ -11,7 +11,8 @@ public class Game2Agent : Agent
     private float greatestTimeAlive = -1f;
     private float timeAlive = 0f;
 
-    private Vector3[] closestAsteroids;
+    private Vector2[,] closestAsteroids;
+    private Vector2[,] bullets;
 
     public Text scoreText;
     public int score = 0;
@@ -35,7 +36,7 @@ public class Game2Agent : Agent
     private float shootTime;
     private float previousX, previousY;
 
-    public override void AgentAction(float [] vectorAction, string textAction)
+    public override void AgentAction(float[] vectorAction, string textAction)
     {
         if (vectorAction[3] == 1f && shootTime <= 0)
         {
@@ -72,18 +73,18 @@ public class Game2Agent : Agent
         {
             movement.x = 0;
         }
-        if(vectorAction[2] == 1f)
+        if (vectorAction[2] == 1f)
         {
             //Rotate CCW
             rb.rotation += 1 * turnSpeed;
         }
-        else if(vectorAction[2] == 2f)
+        else if (vectorAction[2] == 2f)
         {
             //Rotate CW
             rb.rotation += -1 * turnSpeed;
         }
         Mathf.Clamp(rb.rotation, 0, 360);
-        rb.AddForce(movement * speed/30 + -(rb.velocity * Time.deltaTime/50));
+        rb.AddForce(movement * speed / 30 + -(rb.velocity * Time.deltaTime / 50));
         Vector3 pos = cam.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
@@ -91,15 +92,15 @@ public class Game2Agent : Agent
         AddReward(-1f / agentParameters.maxStep);
 
     }
-    private Vector3[] GetClosestAsteroids()
+    private Vector2[,] GetClosestAsteroids()
     {
-        closestAsteroids = new Vector3[5];
+        closestAsteroids = new Vector2[5, 2]; //5 possible asteroids with 2 attributes per asteroid (position, velocity)
         List<GameObject> allAsteroids = GetAllTagged(this.transform, "asteroid");
-        
+
         float shortestDistance = Mathf.Infinity;
         GameObject closestAsteroid = null;
-        for (int i = 0; i < closestAsteroids.Length; i++)
-        { 
+        for (int i = 0; i < closestAsteroids.GetLength(0); i++)
+        {
             foreach (GameObject asteroid in allAsteroids)
             {
                 float currentDistance = Vector3.Distance(asteroid.transform.position, transform.position);
@@ -111,17 +112,55 @@ public class Game2Agent : Agent
             }
             if (closestAsteroid != null)
             {
-                closestAsteroids[i] = closestAsteroid.transform.position;
-                closestAsteroids[i][0] = (transform.parent.parent.position.x - closestAsteroids[i][0]) / 8.5f; //normalized to bounds of canvas
-                closestAsteroids[i][1] = (transform.parent.parent.position.y - closestAsteroids[i][1]) / 4.8f; //normalized to bounds of canvas
-                closestAsteroids[i][2] = 0;
+                closestAsteroids[i, 0] = new Vector2(closestAsteroid.transform.position[0], closestAsteroid.transform.position[1]);
+                closestAsteroids[i, 0][0] = (closestAsteroids[i, 0][0] - transform.parent.parent.position.x) / 44f; //normalized to bounds of canvas
+                closestAsteroids[i, 0][1] = (closestAsteroids[i, 0][1] - transform.parent.parent.position.y) / 40f; //normalized to bounds of canvas
+                closestAsteroids[i, 1] = new Vector2(closestAsteroid.GetComponent<Rigidbody2D>().velocity[0], closestAsteroid.GetComponent<Rigidbody2D>().velocity[1]);
+                closestAsteroids[i, 1][0] = closestAsteroids[i, 1][0] / Asteroid.speed; //normalized to asteroid speed given by curricula
+                closestAsteroids[i, 1][1] = closestAsteroids[i, 1][1] / Asteroid.speed; //normalized to asteroid speed given by curricula
+
                 //Debug.Log("Asteroids: " + closestAsteroid);
                 allAsteroids.Remove(closestAsteroid);
             }
         }
-        
+        //2 dimensional array of position, velocity of asteroids
         return closestAsteroids;
     }
+    private Vector2[,] GetActiveBullets()
+    {
+        bullets = new Vector2[2, 2]; //5 possible asteroids with 2 attributes per asteroid (position, velocity)
+        List<GameObject> allBullets = GetAllTagged(this.transform, "ball");
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject closestBullet = null;
+        for (int i = 0; i < bullets.GetLength(0); i++)
+        {
+            foreach (GameObject bullet in allBullets)
+            {
+                float currentDistance = Vector3.Distance(bullet.transform.position, transform.position);
+                if (currentDistance < shortestDistance)
+                {
+                    closestBullet = bullet;
+                    shortestDistance = currentDistance;
+                }
+            }
+            if (closestBullet != null)
+            {
+                bullets[i, 0] = new Vector2(closestBullet.transform.position[0], closestBullet.transform.position[1]);
+                bullets[i, 0][0] = (bullets[i, 0][0] - transform.parent.parent.position.x) / 45f; // normalized to bound within bullet barriers
+                bullets[i, 0][1] = (bullets[i, 0][1] - transform.parent.parent.position.y) / 27f; // normalized to bound within bullet barriers
+                bullets[i, 1] = new Vector2(closestBullet.GetComponent<Rigidbody2D>().velocity[0], closestBullet.GetComponent<Rigidbody2D>().velocity[1]);
+                bullets[i, 1][0] = bullets[i, 1][0] / (bulletSpeed * 2f); // normalized to max speed of bullet
+                bullets[i, 1][1] = bullets[i, 1][1] / (bulletSpeed * 2f); // normalized to max speed of bullet
+
+                //Debug.Log("Asteroids: " + closestAsteroid);
+                allBullets.Remove(closestBullet);
+            }
+        }
+        //2 dimensional array of position, velocity of asteroids
+        return bullets;
+    }
+
     private List<GameObject> GetAllTagged(Transform parent, string tag)
     {
         //searches down hierarchy for specific tagged GameObjs (Does not have to be in play area)
@@ -138,25 +177,45 @@ public class Game2Agent : Agent
     }
     public override void CollectObservations()
     {
-        AddVectorObs(rb.velocity/8.3f); //normalized 8.3f is the max velocity possible
-        AddVectorObs(transform.rotation.z); //normalized
+        //\\=====All observations are normalized from the range of [-1 , +1]=====//\\ 
+        //Player Velocity and Current Rotation
+        AddVectorObs(rb.velocity / 8.3f); //normalized 8.3f is the max velocity possible
+        AddVectorObs(transform.rotation.z);
 
-        AddVectorObs((transform.parent.parent.position.x - transform.position.x) / 8.5f); //normalized
-        AddVectorObs((transform.parent.parent.position.y - transform.position.y) / 4.8f); //normalized
+        //Player Position
+        AddVectorObs((transform.position.x - transform.parent.parent.position.x) / 8.5f);
+        AddVectorObs((transform.position.y - transform.parent.parent.position.y) / 4.8f);
 
-        Vector3 [] incomingAsteroids = GetClosestAsteroids(); //normalized positions
+        Vector2[,] incomingAsteroids = GetClosestAsteroids();
+
         //Asteroid Positions
-        AddVectorObs(incomingAsteroids[0]);
-        AddVectorObs(incomingAsteroids[1]);
-        AddVectorObs(incomingAsteroids[2]);
-        AddVectorObs(incomingAsteroids[3]);
-        AddVectorObs(incomingAsteroids[4]);
+        AddVectorObs(incomingAsteroids[0, 0]);
+        AddVectorObs(incomingAsteroids[1, 0]);
+        AddVectorObs(incomingAsteroids[2, 0]);
+        AddVectorObs(incomingAsteroids[3, 0]);
+        AddVectorObs(incomingAsteroids[4, 0]);
+        //Asteroid Velocities
+        AddVectorObs(incomingAsteroids[0, 1]);
+        AddVectorObs(incomingAsteroids[1, 1]);
+        AddVectorObs(incomingAsteroids[2, 1]);
+        AddVectorObs(incomingAsteroids[3, 1]);
+        AddVectorObs(incomingAsteroids[4, 1]);
 
-        AddVectorObs((transform.parent.parent.position.x - spawnPointScript.currentPosition.x) / 8.5f); //normalized(bounds of game area)
-        AddVectorObs((transform.parent.parent.position.y - spawnPointScript.currentPosition.y) / 4.8f); //normalized (bounds of game area)
+        Vector2[,] activeBullets = GetActiveBullets();
+        //Bullet Positions
+        AddVectorObs(activeBullets[0, 0]);
+        AddVectorObs(activeBullets[1, 0]);
+        //Bullet Velocities
+        //AddVectorObs(activeBullets[0, 1]);
+        //AddVectorObs(activeBullets[1, 1]);
 
+        //Powerup Position
+        AddVectorObs((spawnPointScript.currentPosition.x - transform.parent.parent.position.x) / 8.5f);
+        AddVectorObs((spawnPointScript.currentPosition.y - transform.parent.parent.position.y) / 4.8f);
+
+        //Raycasts
         float[] angles = { 0f, 45f, 60f, 75f, 90f, 105f, 120f, 135f, 180f, 225f, 270f, 315f };
-        string[] tags = { "asteroid", "wall" , "goal"};
+        string[] tags = { "asteroid", "wall", "goal" };
         AddVectorObs(rayPerception.Perceive(10f, angles, tags));
     }
 
@@ -213,7 +272,7 @@ public class Game2Agent : Agent
             Done();
             this.score--;
             //Either kill player here or decrement his score
-            
+
         }
     }
 
@@ -226,11 +285,11 @@ public class Game2Agent : Agent
 
     public void Shoot()
     {
-        GameObject bulletTemp = Instantiate(bulletPrefab, point.position, point.rotation);
+        GameObject bulletTemp = Instantiate(bulletPrefab, point.position, point.rotation, transform);
         Rigidbody2D rb = bulletTemp.GetComponent<Rigidbody2D>();
         rb.AddForce(point.up * bulletSpeed, ForceMode2D.Impulse);
     }
-    
+
     private float GetSpeed()
     {
         float speed = Mathf.Sqrt(Mathf.Abs(transform.position.x - previousX) + Mathf.Abs(transform.position.y - previousY));
