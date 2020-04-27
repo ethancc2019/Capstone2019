0c07ec7aab1844ac259b1da59b62cff6f39782aa
Ethan C <ethan.collins335@myci.csuci.edu>
2020-04-27 11:50:42 -0700

    Bug fix for power ups

--- ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #3/Scripts/PowerUpController.cs
+++ ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/CAPSTONE/Game #3/Scripts/PowerUpController.cs
@@ -25,13 +25,14 @@ public class PowerUpController : MonoBehaviour
         if (collider.CompareTag("Player"))
         {
             Debug.Log("+5 Ammo Added!");
-            playerGameObject = GameObject.FindGameObjectWithTag("Player"); //This may need to be fixed
+            playerGameObject = this.transform.parent.Find("Player_1").gameObject;
             playerGameObject.GetComponent<Shooting>().ammoCount += 5;
-            GameObject.FindGameObjectWithTag("power_container").GetComponent<PowerUpSpawnGameThree>().activePowerUps--;
-            //Debug.Log(GameObject.FindGameObjectWithTag("power_container").GetComponent<PowerUpSpawnGameThree>().activePowerUps.ToString());
-            //Add +5 ammo to the player referencing his Shooting script
-            GameObject temp = gameObject;
-            Destroy(temp);
+            //GameObject.FindGameObjectWithTag("power_container").GetComponent<PowerUpSpawnGameThree>().activePowerUps--;
+            GameObject tempPowerUp = this.gameObject;
+            GameObject tempContainer = this.transform.parent.Find("Power Up Container").gameObject;
+            tempContainer.GetComponent<PowerUpSpawnGameThree>().activePowerUps--;
+            //GameObject temp = gameObject;
+            Destroy(tempPowerUp);
         }
             
         
